fbd79a94e884cfa5b3bd6cc2fdded7f7e8bd88bc
Nikoletta Toth <tothniki.ntt@gmail.com>
2019-11-19 23:03:11 +0100

    Addimage view for the clothes.

--- app/src/main/java/com/example/kata/clothes/ui/detail/MyClothesRecyclerViewAdapter.java
+++ app/src/main/java/com/example/kata/clothes/ui/detail/MyClothesRecyclerViewAdapter.java
@@ -1,9 +1,11 @@
 package com.example.kata.clothes.ui.detail;
 
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import com.example.kata.clothes.R;
 
@@ -19,7 +21,7 @@ import java.util.List;
  * TODO: Replace the implementation with code for your data type.
  */
 public class MyClothesRecyclerViewAdapter extends RecyclerView.Adapter<MyClothesRecyclerViewAdapter.ViewHolder> {
-
+    private int height;
     private final List<ClothesModel> mValues;
     private final OnListFragmentInteractionListener mListener;
 
@@ -39,7 +41,11 @@ public class MyClothesRecyclerViewAdapter extends RecyclerView.Adapter<MyClothes
     public void onBindViewHolder(final ViewHolder holder, int position) {
         holder.mItem = mValues.get(position);
         //holder.mIdView.setText(mValues.get(position).getId().toString());
-        holder.mContentView.setText(mValues.get(position).getName());
+        //holder.mContentView.setText(mValues.get(position).getName());
+        String uriString = mValues.get(position).getUri();
+        if(uriString != null && !uriString.isEmpty()){
+            holder.mImageView.setImageURI(Uri.parse(uriString));
+        }
 
         holder.mView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,19 +67,21 @@ public class MyClothesRecyclerViewAdapter extends RecyclerView.Adapter<MyClothes
     public class ViewHolder extends RecyclerView.ViewHolder {
         public final View mView;
         //public final TextView mIdView;
-        public final TextView mContentView;
+        //public final TextView mContentView;
+        public final ImageView mImageView;
         public ClothesModel mItem;
 
         public ViewHolder(View view) {
             super(view);
             mView = view;
             //mIdView = view.findViewById(R.id.cloth_item_number);
-            mContentView = view.findViewById(R.id.cloth_content);
+            //mContentView = view.findViewById(R.id.cloth_content);
+            mImageView = view.findViewById((R.id.cloth_content_iv));
         }
 
-        @Override
-        public String toString() {
-            return super.toString() + " '" + mContentView.getText() + "'";
-        }
+//        @Override
+//        public String toString() {
+//            return super.toString() + " '" + mContentView.getText() + "'";
+//        }
     }
 }
